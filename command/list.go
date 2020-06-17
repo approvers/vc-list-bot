392ac84532864e743e312a10701a6ddefa0f28b7
unios103 <unios103@gmail.com>
2020-06-17 22:41:29 +0900

    ファイル分け

---
+++ command/list.go
@@ -0,0 +1,15 @@
+package command
+
+import(
+	// "fmt"
+	"github.com/bwmarrin/discordgo"
+)
+
+func List(session *discordgo.Session, message *discordgo.MessageCreate){
+	if message.Author.ID == session.State.User.ID {
+		return
+	}
+	if message.Content == "!list" {
+		session.ChannelMessageSend(message.ChannelID, "Pong!")
+	}
+}
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import(
 
 	"github.com/joho/godotenv"
 	"github.com/bwmarrin/discordgo"
+
+	"./command"
 )
 
 func main() {
@@ -23,27 +25,17 @@ func main() {
 		fmt.Println("error creating Discord session,", err)
 		return
 	}
-	discord.AddHandler(messageCreate)
+
+	discord.AddHandler(command.List)
 	err = discord.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
+
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 	discord.Close()
-}
-
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
 }
